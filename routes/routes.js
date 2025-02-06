5b9305868a44e08b57f7200346361ec2ce0c6f2f
soumick727 <roysoumick727@gmail.com>
2025-02-07 01:09:52 +0530

    changes made

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const User = require('../models/user');
 
-router.get("/user",(req,res)=>{
-    res.send("All users");
+//image upload
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.filename + '_' + Date.now() + '_' + file.originalname);
+    }
+});
+
+var upload  = multer({
+    storage: storage,
+}).single("image");
+
+router.get("/",(req,res)=>{
+    res.render('home',{title: 'Home Page'});
+})
+
+router.get("/add",(req,res)=>{
+    res.render('add',{title: 'Add Page'});
 })
 
 module.exports = router;
